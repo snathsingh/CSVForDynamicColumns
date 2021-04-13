3fb33a2c93da2d24eadf738178ce8eec9bf7a81a
Shakti <xyz@gmail.com>
2021-04-13 12:57:06 +0530

    CSV creation

---
+++ ConsoleAppAshish/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ConsoleAppAshish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cs = "server=.;initial catalog=Dev_Demo2019;integrated security=SSPI";
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("select * from BufferEmployee", con);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                DataTable dt = ds.Tables[0];
+                dt.ToCSV("D://ShaktiSing.csv");                
+            }
+        }
+    }
+}
